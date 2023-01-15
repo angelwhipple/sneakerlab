857f10deb9cb982164579372dbe8e9c2673a0fac
Whipple <whipple@dhcp-10-29-72-37.dyn.mit.edu>
2023-01-15 03:57:31 -0500

    linked mongo db

--- client/src/components/pages/Discover.js
+++ client/src/components/pages/Discover.js
@@ -30,7 +30,6 @@ const Discover = ({ userId, handleLogin, handleLogout }) => {
         </div>
       </div>
       <h1>Good luck on your project :)</h1>
-      <h2> What you need to change in this skeleton</h2>
       <h2>How to go from this skeleton to our actual app</h2>
       <a href="https://docs.google.com/document/d/110JdHAn3Wnp3_AyQLkqH2W8h5oby7OVsYIeHYSiUzRs/edit?usp=sharing">
         Check out this getting started guide
