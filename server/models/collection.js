4ec9f1730f08b71278a5b4430bece025dd871c7e
Angel Whipple <awhipp@mit.edu>
2023-01-15 03:44:15 -0500

    Delete server directory

--- server/models/collection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const CollectionSchema = new mongoose.Schema({
-  collection_name: String,
-  shoe_urls: [String],
-});
-
-module.exports = mongoose.model("collection", CollectionSchema);
--- server/models/item.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const ShoeSchema = new mongoose.Schema({
-  userId: String,
-  shoe_url: String,
-  tag: String,
-});
-
-module.exports = mongoose.model("item", ShoeSchema);
