99c8264f91cd6ccb2eb2bd7e8fdddd158b7fc452
Xenia Zhao <xeniazhao76@gmail.com>
2023-01-17 17:47:12 -0500

    add CollectionDetail

---
+++ client/src/components/pages/CollectionDetail.js
@@ -0,0 +1,13 @@
+import { get } from "../../utilities";
+import React from "react";
+import { useState, useEffect } from "react";
+
+const CollectionDetail = () => {
+
+};
+
+return (
+
+);
+
+export default CollectionDetail;
