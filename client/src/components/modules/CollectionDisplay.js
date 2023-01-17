e373426e204018406e06425d0183e0fad3ab7af2
Xenia Zhao <xeniazhao76@gmail.com>
2023-01-17 17:58:54 -0500

    create collectiondisplay component

---
+++ client/src/components/modules/CollectionDisplay.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+/**
+ * Component to render a single collection for discover or profile
+ * Proptypes
+ * @param {string} creator
+ * @param {string} title
+ * @param {list} shoes
+ */
+const CollectionDisplay = (props) => {
+    return (
+        <div>
+            <Link to={collectiondetail/props._id}>
+                <div className="u-bold">{props.title}</div>
+            </Link>
+        </div>
+    );
+};
+
+export default CollectionDisplay;
